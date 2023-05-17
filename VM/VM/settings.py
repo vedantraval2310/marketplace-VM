c3afc693c8be30555663d16c964bb37c2d717d4e
Vedant Raval <76436755+vedantraval2310@users.noreply.github.com>
2023-05-17 08:40:09 +0530

    Update settings.py

--- VM/VM/settings.py
+++ VM/VM/settings.py
@@ -147,7 +147,7 @@ MESSAGE_TAGS ={
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vedant.k.raval@gmail.com'
-EMAIL_HOST_PASSWORD = 'mtisrmygmnaapsoj'
+EMAIL_HOST_PASSWORD = 'Enter Your generated password'
 EMAIL_USE_TLS = True
 
 
